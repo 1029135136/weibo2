68a8e95ee413b36ac25adf3088797b4313c20347
ANG.YU <1029135136@qq.com>
2020-10-13 03:44:33 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
-{
+{   //设置为可更新的模式
+    protected $fillable = ['content'];
+
     //将文章与用户相互关联
     public function user()
     {
--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,4 @@ Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm
     ->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset')
     ->name('password.update');
+Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
