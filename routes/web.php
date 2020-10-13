7c9bd28603b76489d228909743724e0c4d481814
ANG.YU <1029135136@qq.com>
2020-10-13 02:39:32 +0000

    重置密码

--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,12 @@ Route::get('/users/{user}/edit', 'UserController@edit')
 //邮件激活功能
 Route::get('signup/confirm/{token}', 'UserController@confirmEmail')
     ->name('confirm_email');
+//重置密码功能
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')
+    ->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')
+    ->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')
+    ->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')
+    ->name('password.update');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -9,7 +10,6 @@
  | file for the application as well as bundling up all the JS files.
  |
  */
-const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css').version();
+    .sass('resources/sass/app.scss', 'public/css');
